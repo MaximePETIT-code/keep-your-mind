99f418f62d6406a45f210d4ac0685c9a29bacc3d
Maxime Petit <contactmaximepetit@gmail.com>
2023-03-04 17:00:07 +0100

    :sparkles: 3d rotate and glow effect on hover element

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,10 @@
 import Lenis from "@studio-freight/lenis";
 
+const previews = document.querySelectorAll(".preview");
+
 // Smooth scrolling effect
 let lenis;
+
 
 const initSmoothScrolling = () => {
   // Smooth scrolling initialization
@@ -17,5 +20,44 @@ const initSmoothScrolling = () => {
   requestAnimationFrame(scrollFn);
 };
 
+let rotateFunction; // Variable to store rotate function
+
+function rotateToMouse(e) {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+  const preview = e.target;
+  const previewSize = preview.getBoundingClientRect();
+  const leftX = mouseX - previewSize.x;
+  const topY = mouseY - previewSize.y;
+  const center = {
+    x: leftX - previewSize.width / 2,
+    y: topY - previewSize.height / 2,
+  };
+  const distance = Math.sqrt(center.x ** 2 + center.y ** 2);
+
+  this.style.transform = `
+    scale3d(1.05, 1.05, 1.05)
+    rotate3d(
+      0.5,
+      ${center.x / 100},
+      0.08,
+      ${Math.log(distance) * 4}deg
+    )
+  `;
+}
+
+previews.forEach(preview => {
+  preview.addEventListener("mouseenter", () => {
+    rotateFunction = rotateToMouse.bind(preview);
+    preview.addEventListener("mousemove", rotateFunction);
+  });
+
+  preview.addEventListener("mouseleave", () => {
+    preview.removeEventListener("mousemove", rotateFunction);
+    preview.style.transform = "";
+    preview.style.opacity = "";
+  });
+});
+
 // After loading of the page
-initSmoothScrolling()
+initSmoothScrolling();
